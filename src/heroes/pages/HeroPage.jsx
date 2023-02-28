7d71e91c6cdff742d4d5a841c98cef03f1ad52a5
OliverosA <aldair_oliveros09@hotmail.es>
2023-02-28 17:28:27 -0600

    reading url params

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,3 +1,14 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
+  const { id } = useParams();
+
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to={"/marvel"} />;
+  }
+
   return <div>HeroPage</div>;
 };
