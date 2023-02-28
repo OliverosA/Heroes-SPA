d40de2d93dcac50cc828582dc294ba169de27d79
OliverosA <aldair_oliveros09@hotmail.es>
2023-02-28 16:02:06 -0600

    heroes data added

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,3 +1,11 @@
+import { HeroList } from "../components/HeroList";
+
 export const DcPage = () => {
-  return <h1>DcPage</h1>;
+  return (
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+      <HeroList publisher={"DC Comics"} />
+    </>
+  );
 };
